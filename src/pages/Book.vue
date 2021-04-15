4b4bb0906d04a0249dccd858fd3c91f84c22dbe5
the-einstein <quyosh.ogli@gmail.com>
2021-04-15 10:00:33 +0500

    feat: add book page inner elements

--- src/pages/Book.vue
+++ src/pages/Book.vue
@@ -1,12 +1,18 @@
 <template>
-<div>
-
+<div id="book">
+  <span id="title">{{book.volumeInfo.title}}</span>
+  <img id="thumbnail" :src="book.volumeInfo.imageLinks.smallThumbnail" alt="">
+  <span id="description">{{book.searchInfo.textSnippet}}</span>
+  <span id="authors">{{book.volumeInfo.authors}}</span>
 </div>
 </template>
 
 <script>
 export default {
-  name: "Book"
+  name: "Book",
+  props: [
+      'book'
+  ]
 }
 </script>
 
